78e349a9514692bbb94bbe5a999c56e901d0260c
Kalaimani <kalaimani296@gmail.com>
2018-05-15 11:44:32 +0530

    first commit for cucumber tutuorial

---
+++ output/report.js
@@ -0,0 +1,449 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("\u0027stepdefin\\dailyroutine.feature\u0027");
+formatter.feature({
+  "line": 1,
+  "name": "Life of king",
+  "description": "\r\nIn order to go office\r\nAs a working guy\r\nI want to wish people",
+  "id": "life-of-king",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 31,
+  "name": "I am a kingman",
+  "description": "",
+  "id": "life-of-king;i-am-a-kingman;;2",
+  "type": "scenario",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 7,
+      "name": "@tester"
+    }
+  ]
+});
+formatter.step({
+  "line": 10,
+  "name": "I work in Latenights",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 11,
+  "name": "I meet watchmen",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 12,
+  "name": "I Greeted him",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 14,
+  "name": "I work in Mornings",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 15,
+  "name": "I meet NewspaperBoy",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 16,
+  "name": "I skipGreeting him",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 18,
+  "name": "I am on e-buyer homepage",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 19,
+  "name": "I click the search button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 20,
+  "name": "Search result should display different devices",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 25,
+  "name": "we work in \"tenpm\"",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 26,
+  "name": "we meet \"watchmen\"",
+  "matchedColumns": [
+    1
+  ],
+  "keyword": "When "
+});
+formatter.step({
+  "line": 27,
+  "name": "I skipGreeting him",
+  "keyword": "Then "
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Latenights",
+      "offset": 10
+    }
+  ],
+  "location": "AtitudeTest.ln(String)"
+});
+formatter.result({
+  "duration": 162923647,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "watchmen",
+      "offset": 7
+    }
+  ],
+  "location": "AtitudeTest.Imeet(String)"
+});
+formatter.result({
+  "duration": 131641,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Greeted",
+      "offset": 2
+    }
+  ],
+  "location": "AtitudeTest.gre(String)"
+});
+formatter.result({
+  "duration": 110222,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Mornings",
+      "offset": 10
+    }
+  ],
+  "location": "AtitudeTest.ln(String)"
+});
+formatter.result({
+  "duration": 103529,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "NewspaperBoy",
+      "offset": 7
+    }
+  ],
+  "location": "AtitudeTest.Imeet(String)"
+});
+formatter.result({
+  "duration": 103082,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "skipGreeting",
+      "offset": 2
+    }
+  ],
+  "location": "AtitudeTest.gre(String)"
+});
+formatter.result({
+  "duration": 104421,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AtitudeTest.I_am_on_homepage()"
+});
+formatter.result({
+  "duration": 75415,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "search",
+      "offset": 12
+    }
+  ],
+  "location": "AtitudeTest.click_search(String)"
+});
+formatter.result({
+  "duration": 119593,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "different devices",
+      "offset": 29
+    }
+  ],
+  "location": "AtitudeTest.search_devices(String)"
+});
+formatter.result({
+  "duration": 115131,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "tenpm",
+      "offset": 12
+    }
+  ],
+  "location": "AtitudeTest.we_work_in(String)"
+});
+formatter.result({
+  "duration": 99958,
+  "status": "passed"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "skipGreeting",
+      "offset": 2
+    }
+  ],
+  "location": "AtitudeTest.gre(String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.scenario({
+  "line": 32,
+  "name": "I am a kingman",
+  "description": "",
+  "id": "life-of-king;i-am-a-kingman;;3",
+  "type": "scenario",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 7,
+      "name": "@tester"
+    }
+  ]
+});
+formatter.step({
+  "line": 10,
+  "name": "I work in Latenights",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 11,
+  "name": "I meet watchmen",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 12,
+  "name": "I Greeted him",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 14,
+  "name": "I work in Mornings",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 15,
+  "name": "I meet NewspaperBoy",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 16,
+  "name": "I skipGreeting him",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 18,
+  "name": "I am on e-buyer homepage",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 19,
+  "name": "I click the search button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 20,
+  "name": "Search result should display different devices",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 25,
+  "name": "we work in \"ninepm\"",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 26,
+  "name": "we meet \"cabdriver\"",
+  "matchedColumns": [
+    1
+  ],
+  "keyword": "When "
+});
+formatter.step({
+  "line": 27,
+  "name": "I skipGreeting him",
+  "keyword": "Then "
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Latenights",
+      "offset": 10
+    }
+  ],
+  "location": "AtitudeTest.ln(String)"
+});
+formatter.result({
+  "duration": 142798,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "watchmen",
+      "offset": 7
+    }
+  ],
+  "location": "AtitudeTest.Imeet(String)"
+});
+formatter.result({
+  "duration": 103529,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Greeted",
+      "offset": 2
+    }
+  ],
+  "location": "AtitudeTest.gre(String)"
+});
+formatter.result({
+  "duration": 104867,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Mornings",
+      "offset": 10
+    }
+  ],
+  "location": "AtitudeTest.ln(String)"
+});
+formatter.result({
+  "duration": 105313,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "NewspaperBoy",
+      "offset": 7
+    }
+  ],
+  "location": "AtitudeTest.Imeet(String)"
+});
+formatter.result({
+  "duration": 239187,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "skipGreeting",
+      "offset": 2
+    }
+  ],
+  "location": "AtitudeTest.gre(String)"
+});
+formatter.result({
+  "duration": 230708,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AtitudeTest.I_am_on_homepage()"
+});
+formatter.result({
+  "duration": 209735,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "search",
+      "offset": 12
+    }
+  ],
+  "location": "AtitudeTest.click_search(String)"
+});
+formatter.result({
+  "duration": 233386,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "different devices",
+      "offset": 29
+    }
+  ],
+  "location": "AtitudeTest.search_devices(String)"
+});
+formatter.result({
+  "duration": 119593,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "ninepm",
+      "offset": 12
+    }
+  ],
+  "location": "AtitudeTest.we_work_in(String)"
+});
+formatter.result({
+  "duration": 96389,
+  "status": "passed"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "skipGreeting",
+      "offset": 2
+    }
+  ],
+  "location": "AtitudeTest.gre(String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+});
